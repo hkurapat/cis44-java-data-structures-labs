6cee27272b505b314ac321e4063b6c1132324daa
Harshil Kurapati <ckurapat@gmail.com>
2026-05-01 23:16:03 -0700

    Create MyEntry.java

---
+++ lab13-Priority_Queue/MyEntry.java
@@ -0,0 +1,22 @@
+// this holds the key and value pair just like the Entry ADT from the lecture slides
+public class MyEntry<K extends Comparable<K>, V> implements Comparable<MyEntry<K, V>> {
+    private K key;
+    private V value;
+
+    public MyEntry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    public K getKey() { return key; }
+    public V getValue() { return value; }
+
+    @Override
+    public int compareTo(MyEntry<K, V> other) {
+        return this.key.compareTo(other.key);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + key + ", " + value + ")";
+    }
+}
